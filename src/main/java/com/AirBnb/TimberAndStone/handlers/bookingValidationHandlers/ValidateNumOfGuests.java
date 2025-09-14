20c19bd02c55cd0227414d303d85652adb155214
SatangSagwa <emilia.ivarsson@live.se>
2025-09-14 13:28:08 +0200

    Created class for ValidateNumOfGuests

---
+++ src/main/java/com/AirBnb/TimberAndStone/handlers/bookingValidationHandlers/ValidateNumOfGuests.java
@@ -0,0 +1,30 @@
+package com.AirBnb.TimberAndStone.handlers.bookingValidationHandlers;
+
+import com.AirBnb.TimberAndStone.dtos.requests.booking.BookingRequest;
+import com.AirBnb.TimberAndStone.validation.RentalValidation;
+
+
+// Checks if the booking requests num of guests is less than 1
+// or greater than the rentals' capacity.
+public class ValidateNumOfGuests implements BookingValidatorHandler{
+    private BookingValidatorHandler nextHandler;
+    private RentalValidation rentalValidation;
+
+
+    @Override
+    public void handleRequest(BookingRequest request) {
+        if (request.getNumberOfGuests() < 1) {
+            throw new IllegalArgumentException("Number of guests must be greater than 0");
+        } else if (request.getNumberOfGuests() > request.getRental().getCapacity()) {
+            throw new IllegalArgumentException("Number of guests must be less than the capacity");
+        } else if (nextHandler != null) {
+            nextHandler.handleRequest(request);
+        }
+
+    }
+
+    @Override
+    public void setNextHandler(BookingValidatorHandler nextHandler) {
+        this.nextHandler = nextHandler;
+    }
+}
