14f36be5061cb76586164c9351e15d38219a6d4c
karolina-svensson <karolinasvensson92@gmail.com>
2025-09-15 11:26:01 +0200

    added ValidateRentalPeriod for Chain of Responsibility and started code toa test all was good

---
+++ src/main/java/com/AirBnb/TimberAndStone/handlers/bookingValidationHandlers/ValidateRentalPeriod.java
@@ -0,0 +1,29 @@
+package com.AirBnb.TimberAndStone.handlers.bookingValidationHandlers;
+
+import com.AirBnb.TimberAndStone.dtos.requests.booking.BookingRequest;
+import com.AirBnb.TimberAndStone.services.PeriodService;
+
+public class ValidateRentalPeriod implements BookingValidatorHandler {
+    private BookingValidatorHandler nextHandler;
+    private final PeriodService periodService;
+
+    public ValidateRentalPeriod(PeriodService periodService) {
+        this.periodService = periodService;
+    }
+    @Override
+    public void handleRequest(BookingRequest request) {
+        // periodService.isPeriodMatching checks available periods in a rental
+        periodService.isPeriodMatching(request.getRental().getAvailablePeriods(), request);
+        // periodService.isPeriodMatching(rental.getAvailablePeriods(), request);
+
+        if (nextHandler != null) {
+            nextHandler.handleRequest(request);
+        }
+    }
+
+    @Override
+    public void setNextHandler(BookingValidatorHandler nextHandler) {
+        this.nextHandler = nextHandler;
+    }
+
+}
